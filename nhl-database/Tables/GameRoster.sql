a08a2693905774e8f9042ad441e2e640a9e8081a
Cole Titze <cole.titze@outlook.com>
2022-11-19 22:19:43 -0600

    Fixed foreign keys

--- nhl-database/Tables/GameRoster.sql
+++ nhl-database/Tables/GameRoster.sql
@@ -3,8 +3,9 @@ CREATE TABLE [dbo].[GameRoster]
     gameId INT NOT NULL,
     teamId INT NOT NULL,
     playerId INT NOT NULL,
+    seasonStartYear INT NOT NULL
     PRIMARY KEY(gameId),
     FOREIGN KEY (gameId) REFERENCES Game(id),
     FOREIGN KEY (teamId) REFERENCES Team(id),
-    FOREIGN KEY (teamId) REFERENCES PlayerValue(id),
+    FOREIGN KEY (playerId,seasonStartYear) REFERENCES PlayerValue(id,seasonStartYear),
 );
