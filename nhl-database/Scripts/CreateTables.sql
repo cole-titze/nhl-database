aded36a3d781c27dd650052f8b1c9082f5f3727c
Cole <cole.titze@outlook.com>
2023-10-19 18:27:54 -0500

    Update log loss table to new bookmakers

--- nhl-database/Scripts/CreateTables.sql
+++ nhl-database/Scripts/CreateTables.sql
@@ -43,11 +43,9 @@ CREATE TABLE [dbo].[Game]
 CREATE TABLE [dbo].[LogLossGame]
 (
     gameId INT NOT NULL,
-    bovadaLogLoss FLOAT NOT NULL DEFAULT 0,
+    draftKingsLogLoss FLOAT NOT NULL DEFAULT 0,
     myBookieLogLoss FLOAT NOT NULL DEFAULT 0,
-    pinnacleLogLoss FLOAT NOT NULL DEFAULT 0,
-    betOnlineLogLoss FLOAT NOT NULL DEFAULT 0,
-    bet365LogLoss FLOAT NOT NULL DEFAULT 0,
+    betMgmLogLoss FLOAT NOT NULL DEFAULT 0,
     modelLogLoss FLOAT NOT NULL DEFAULT 0,
     PRIMARY KEY(gameId),
     FOREIGN KEY (gameId) REFERENCES Game(id),
--- nhl-database/Tables/GameLogLoss.sql
+++ nhl-database/Tables/GameLogLoss.sql
@@ -1,11 +1,9 @@
 ﻿CREATE TABLE [dbo].[LogLossGame]
 (
     gameId INT NOT NULL,
-    bovadaLogLoss FLOAT NOT NULL DEFAULT 0,
+    draftKingsLogLoss FLOAT NOT NULL DEFAULT 0,
     myBookieLogLoss FLOAT NOT NULL DEFAULT 0,
-    pinnacleLogLoss FLOAT NOT NULL DEFAULT 0,
-    betOnlineLogLoss FLOAT NOT NULL DEFAULT 0,
-    bet365LogLoss FLOAT NOT NULL DEFAULT 0,
+    betMgmLogLoss FLOAT NOT NULL DEFAULT 0,
     modelLogLoss FLOAT NOT NULL DEFAULT 0,
     PRIMARY KEY(gameId),
     FOREIGN KEY (gameId) REFERENCES Game(id),
