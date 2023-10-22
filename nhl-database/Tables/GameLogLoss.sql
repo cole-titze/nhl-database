6efd75ddeb87ce246305358752e6c9cfb6fe0db6
Cole Titze <cole.titze@outlook.com>
2023-10-21 22:41:14 -0500

    Update tables to new sportsbooks

--- nhl-database/Tables/GameLogLoss.sql
+++ nhl-database/Tables/GameLogLoss.sql
@@ -2,8 +2,9 @@
 (
     gameId INT NOT NULL,
     draftKingsLogLoss FLOAT NOT NULL DEFAULT 0,
-    myBookieLogLoss FLOAT NOT NULL DEFAULT 0,
+    bovadaLogLoss FLOAT NOT NULL DEFAULT 0,
     betMgmLogLoss FLOAT NOT NULL DEFAULT 0,
+    barstoolLogLoss FLOAT NOT NULL DEFAULT 0,
     modelLogLoss FLOAT NOT NULL DEFAULT 0,
     PRIMARY KEY(gameId),
     FOREIGN KEY (gameId) REFERENCES Game(id),
